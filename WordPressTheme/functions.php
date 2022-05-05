794aefa20139ed338b1dfbbada82258d4352d01d
rei <reiikedzero@gmail.com>
2022-05-06 00:38:00 +0900

    function.php変更

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Functions
  */
@@ -9,9 +10,9 @@
  * @codex https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/add_theme_support
  */
 function my_setup() {
-	add_theme_support( 'post-thumbnails' ); /* アイキャッチ */
-	add_theme_support( 'automatic-feed-links' ); /* RSSフィード */
-	add_theme_support( 'title-tag' ); /* タイトルタグ自動生成 */
+	add_theme_support('post-thumbnails'); /* アイキャッチ */
+	add_theme_support('automatic-feed-links'); /* RSSフィード */
+	add_theme_support('title-tag'); /* タイトルタグ自動生成 */
 	add_theme_support(
 		'html5',
 		array( /* HTML5のタグで出力 */
@@ -23,7 +24,7 @@ function my_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'my_setup' );
+add_action('after_setup_theme', 'my_setup');
 
 
 
@@ -32,13 +33,15 @@ add_action( 'after_setup_theme', 'my_setup' );
  *
  * @codex https://wpdocs.osdn.jp/%E3%83%8A%E3%83%93%E3%82%B2%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%83%A1%E3%83%8B%E3%83%A5%E3%83%BC
  */
-function my_script_init()
-{
-
-	wp_enqueue_style( 'my', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.1', 'all' );
-
-	wp_enqueue_script( 'my', get_template_directory_uri() . '/assets/js/script.js', array( 'jquery' ), '1.0.1', true );
-
+function my_script_init() {
+	// WordPress提供のjquery.jsを読み込まない
+	wp_deregister_script('jquery');
+	// jQueryの読み込み
+	wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.6.0.min.js', "", "1.0.1");
+	// 自作cssファイルの読み込み
+	wp_enqueue_style('my', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.1', 'all');
+	// 自作jsファイルの読み込み
+	wp_enqueue_script('my', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '1.0.1', true);
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
 
@@ -93,37 +96,37 @@ add_action('wp_enqueue_scripts', 'my_script_init');
  * @param string $title 書き換え前のタイトル.
  * @return string $title 書き換え後のタイトル.
  */
-function my_archive_title( $title ) {
+function my_archive_title($title) {
 
-	if ( is_home() ) { /* ホームの場合 */
+	if (is_home()) { /* ホームの場合 */
 		$title = 'ブログ';
-	} elseif ( is_category() ) { /* カテゴリーアーカイブの場合 */
-		$title = '' . single_cat_title( '', false ) . '';
-	} elseif ( is_tag() ) { /* タグアーカイブの場合 */
-		$title = '' . single_tag_title( '', false ) . '';
-	} elseif ( is_post_type_archive() ) { /* 投稿タイプのアーカイブの場合 */
-		$title = '' . post_type_archive_title( '', false ) . '';
-	} elseif ( is_tax() ) { /* タームアーカイブの場合 */
-		$title = '' . single_term_title( '', false );
-	} elseif ( is_search() ) { /* 検索結果アーカイブの場合 */
-		$title = '「' . esc_html( get_query_var( 's' ) ) . '」の検索結果';
-	} elseif ( is_author() ) { /* 作者アーカイブの場合 */
+	} elseif (is_category()) { /* カテゴリーアーカイブの場合 */
+		$title = '' . single_cat_title('', false) . '';
+	} elseif (is_tag()) { /* タグアーカイブの場合 */
+		$title = '' . single_tag_title('', false) . '';
+	} elseif (is_post_type_archive()) { /* 投稿タイプのアーカイブの場合 */
+		$title = '' . post_type_archive_title('', false) . '';
+	} elseif (is_tax()) { /* タームアーカイブの場合 */
+		$title = '' . single_term_title('', false);
+	} elseif (is_search()) { /* 検索結果アーカイブの場合 */
+		$title = '「' . esc_html(get_query_var('s')) . '」の検索結果';
+	} elseif (is_author()) { /* 作者アーカイブの場合 */
 		$title = '' . get_the_author() . '';
-	} elseif ( is_date() ) { /* 日付アーカイブの場合 */
+	} elseif (is_date()) { /* 日付アーカイブの場合 */
 		$title = '';
-		if ( get_query_var( 'year' ) ) {
-			$title .= get_query_var( 'year' ) . '年';
+		if (get_query_var('year')) {
+			$title .= get_query_var('year') . '年';
 		}
-		if ( get_query_var( 'monthnum' ) ) {
-			$title .= get_query_var( 'monthnum' ) . '月';
+		if (get_query_var('monthnum')) {
+			$title .= get_query_var('monthnum') . '月';
 		}
-		if ( get_query_var( 'day' ) ) {
-			$title .= get_query_var( 'day' ) . '日';
+		if (get_query_var('day')) {
+			$title .= get_query_var('day') . '日';
 		}
 	}
 	return $title;
 };
-add_filter( 'get_the_archive_title', 'my_archive_title' );
+add_filter('get_the_archive_title', 'my_archive_title');
 
 
 /**
@@ -132,10 +135,10 @@ add_filter( 'get_the_archive_title', 'my_archive_title' );
  * @param int $length 変更前の文字数.
  * @return int $length 変更後の文字数.
  */
-function my_excerpt_length( $length ) {
+function my_excerpt_length($length) {
 	return 80;
 }
-add_filter( 'excerpt_length', 'my_excerpt_length', 999 );
+add_filter('excerpt_length', 'my_excerpt_length', 999);
 
 
 /**
@@ -144,8 +147,7 @@ add_filter( 'excerpt_length', 'my_excerpt_length', 999 );
  * @param string $more 変更前の省略記法.
  * @return string $more 変更後の省略記法.
  */
-function my_excerpt_more( $more ) {
+function my_excerpt_more($more) {
 	return '...';
-
 }
-add_filter( 'excerpt_more', 'my_excerpt_more' );
+add_filter('excerpt_more', 'my_excerpt_more');
